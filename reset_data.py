4ee8ef6a4a851fb7d3bad5c061a70624fc046cc8
Eshan Shah <Eshan@Shahs-MacBook-Air.local>
2025-05-02 17:26:28 +0100

    Added functionality to reset data

---
+++ reset_data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Read the CSV file
+df = pd.read_csv("data/gestures.csv")
+
+df = pd.DataFrame()  
+
+df.to_csv("data/gestures.csv", index=False)
+
+print("Data reset complete.")
